5669c705c9e7052dad446109c394b9a7dec51e19
Hiury Gomes <hiury_jp@hotmail.com>
2021-09-30 10:51:21 -0300

    Exemplo Estrutura repetitiva PARA prof Nelo Alves

---
+++ exemplo Est repetitiva PARA/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace exemplo_Est_repetitiva_PARA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N, x, soma;
+
+            N = int.Parse(Console.ReadLine());
+
+            soma = 0;
+            for (int i=1; i<=N; i++)
+            {
+                x = int.Parse(Console.ReadLine());
+                soma = soma + x;
+            }
+            Console.WriteLine(soma);
+            Console.ReadLine();
+        }
+    }
+}
